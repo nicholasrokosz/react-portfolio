02a49883fea2f2388ad44c0020b3b9496164596d
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-16 21:36:53 -0700

    override link styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,13 @@
 import Typography from "typography"
 import lincolnTheme from "typography-theme-lincoln"
 
+lincolnTheme.overrideThemeStyles = () => ({
+  a: {
+    textShadow: "none",
+    backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 1px, ${"#111827"} 1px, ${"#111827"} 2px, rgba(0, 0, 0, 0) 2px)`,
+  },
+})
+
 const typography = new Typography(lincolnTheme)
 
 export const { scale, rhythm, options } = typography
