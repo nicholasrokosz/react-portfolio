5e058b2b3956b2ab9cd88bde864368c5f00f2866
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-16 23:31:35 -0700

    style ProjectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,11 +2,13 @@ import React from "react"
 import "twin.macro"
 
 const ProjectCard = ({ src, title, desc }) => (
-  <div tw="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-    <div tw="flex-shrink-0">
-      <img src={src} alt={title + " screengrab"} tw="h-12 w-12" />
-    </div>
-    <div tw="ml-6 pt-1">
+  <div tw="mx-auto flex flex-col p-6 bg-white rounded-xl shadow-lg p-0">
+    <img
+      src={src}
+      alt={title + " screengrab"}
+      tw="rounded-none w-auto h-64 rounded-t-xl"
+    />
+    <div tw="ml-6 pb-6">
       <h4 tw="text-xl text-gray-900">{title}</h4>
       <p tw="text-base text-gray-600">{desc}</p>
     </div>
