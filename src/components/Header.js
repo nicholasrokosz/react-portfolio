3653a169bd354b5bb36f5db871d7b5aac9b5878d
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-16 23:32:56 -0700

    redesign Header from mobile first

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,28 +11,28 @@ const ListLink = props => (
 const myName = "< Nick Rokosz />"
 
 const Header = () => (
-  <header tw="mb-6 flex flex-col items-center justify-between md:flex-row">
+  <header tw="mb-6 flex flex-col items-center justify-between md:flex-row max-w-screen-md mx-auto">
     <Link to="/" tw="shadow-none bg-none">
       <h1 tw="inline text-3xl">{myName}</h1>
     </Link>
     <ul tw="list-none float-right text-gray-900 flex flex-row items-stretch justify-between">
-      <div tw="border-r border-gray-900">
+      <div tw="md:border-r md:border-gray-900">
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
       </div>
-      <div tw="px-4 flex justify-between">
+      <div tw="w-20 flex justify-between md:ml-4">
         <a href="https://github.com/nicholasrokosz" tw="bg-none">
           <i className="fa fa-github-square" aria-hidden="true"></i>
           {/* GitHub */}
         </a>
         <a
           href="https://www.linkedin.com/in/nick-rokosz-9085191ba/"
-          tw="ml-4 bg-none"
+          tw="md:ml-4 bg-none"
         >
           <i className="fa fa-linkedin-square" aria-hidden="true"></i>
           {/* LinkedIn */}
         </a>
-        <button tw="pl-4">
+        <button tw="md:pl-4">
           <i className="fa fa-moon-o" aria-hidden="true"></i>
         </button>
       </div>
