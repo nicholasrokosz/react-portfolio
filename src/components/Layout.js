58be9afd2d36b845967eab6b1f2b6a9ba5a03a27
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-16 20:18:15 -0700

    add max width md

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import { GlobalStyles } from "twin.macro"
 import Header from "./Header"
 
 const Layout = ({ children }) => (
-  <div tw="my-12 mx-auto px-4 max-w-screen-sm">
+  <div tw="my-12 mx-auto px-4 max-w-screen-md">
     <Header />
     <GlobalStyles />
     {children}
