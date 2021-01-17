3988805ab5e704dbb8615c7002428016fc279545
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-16 17:34:16 -0700

    refactor Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { GlobalStyles } from "twin.macro"
+import Header from "./Header"
 
-const Layout = ({ children, ...rest }) => (
-  <div {...rest}>
+const Layout = ({ children }) => (
+  <div tw="my-12 mx-auto px-4 max-w-screen-sm">
+    <Header />
     <GlobalStyles />
     {children}
   </div>
