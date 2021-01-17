0b840a0cc13d264cb4e13c86269e4915b3802f36
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-16 21:45:19 -0700

    remove html tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,24 +4,22 @@ import Header from "./Header"
 import { Helmet } from "react-helmet"
 
 const Layout = ({ children }) => (
-  <html tw="bg-indigo-50 h-screen">
-    <div tw="my-12 mx-auto px-4 max-w-screen-md">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Nick Rokosz</title>
-        {/* might be slow, test before deployment */}
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css"
-          integrity="sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY="
-          crossorigin="anonymous"
-        />
-      </Helmet>
-      <Header />
-      <GlobalStyles />
-      {children}
-    </div>
-  </html>
+  <body tw="my-12 mx-auto px-4 max-w-screen-md">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Nick Rokosz</title>
+      {/* might be slow, test before deployment */}
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css"
+        integrity="sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY="
+        crossorigin="anonymous"
+      />
+    </Helmet>
+    <Header />
+    <GlobalStyles />
+    {children}
+  </body>
 )
 
 export default Layout
