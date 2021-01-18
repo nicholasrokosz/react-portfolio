39de465930756d947c6e759fa835ac27c8dbe947
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-17 21:51:17 -0700

    style Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import React from "react"
 import "twin.macro"
 
 const Footer = () => (
-  <footer>
-    © 2019 Built by <a href="https://github.com/nicholasrokosz">Nick Rokosz</a>{" "}
-    with Gatsby and Tailwind CSS
+  <footer tw="mt-12 pb-8 text-center">
+    <p tw="text-xs sm:text-base">
+      © 2021 Built by{" "}
+      <a href="https://github.com/nicholasrokosz">Nick Rokosz</a> with Gatsby
+      and Tailwind CSS
+    </p>
   </footer>
 )
 
