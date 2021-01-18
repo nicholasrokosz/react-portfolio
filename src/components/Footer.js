d64fffe0c12a47b22ffb23d74c8c338f984d53cd
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-17 21:08:33 -0700

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "twin.macro"
+
+const Footer = () => (
+  <footer>
+    © 2019 Built by <a href="https://github.com/nicholasrokosz">Nick Rokosz</a>{" "}
+    with Gatsby and Tailwind CSS
+  </footer>
+)
+
+export default Footer
